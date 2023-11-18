e2a5d35a5588ff81fc229b0df63027e50cc3477e
Maxim <jybo@mail.ru>
2023-11-18 23:53:44 +1000

    Add Les_1_Task_3_формат вывода числовых переменных

--- src/main/kotlin/Lesson_1/Task_4.kt
+++ src/main/kotlin/Lesson_1/Task_4.kt
@@ -31,7 +31,7 @@ fun main(){
    val distance: Long = 40_868_600_000
     val age: Byte = 27
     val partOfDay: Float = 0.075F
-    val sec: Int = 6480
+    val sec: Short = 6480
     val partOfYear: Double  = 2.0547945205479453E-4
     val orbit: Int = 327000
 

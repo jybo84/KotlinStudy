11b8305c5f5fe50bc3811233e28276b19c22234d
Maxim <jybo@mail.ru>
2023-11-22 10:03:47 +1000

    KS-3-5 добавлен вариант с использование split

--- src/main/kotlin/Lesson_3/Les_3_Task_5.kt
+++ src/main/kotlin/Lesson_3/Les_3_Task_5.kt
@@ -8,16 +8,34 @@ package Lesson_3
 Необходимо будет сделать обратное. Распарсить строку – разбить ее на 3 составляющих: откуда, куда
 и номер хода, присвоить соответствующим переменным и распечатать по отдельности.
  */
-fun main(){
+fun main() {
     val receiveData = "D2-D4;0"
+
+    // Вариант 1 (subString)
+
+    println("Вариант 1 (subString)")
 
     val moveFrom = receiveData.substringBefore('-')
     println(moveFrom)
 
-    val moveTo = receiveData.substring(3, 6)
+    val moveTo = receiveData.substring(3, 5)
     println(moveTo)
 
     val moveNumber = receiveData.substringAfter(';')
     println(moveNumber)
 
+    // Вариант 2 (split)
+    println()
+    println("Вариант 2 (split)")
+
+    val s1 = receiveData.split("-", ";") // разбиваем строку на строки
+
+    val moveFromUseSplit = s1.get(0)  // присваиваем каждому элементу соответсвующее значение (метод get()  )
+    println(moveFromUseSplit)         // выводим на печать
+
+    val moveToUseSplit = s1[1]  // присваиваем каждому элементу соответсвующее значение (метод array[]   )
+    println(moveToUseSplit)
+
+    val moveNumberUseSplit = s1.get(2)
+    println(moveNumberUseSplit)
 }

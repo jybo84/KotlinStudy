a5d54be67f6530e32177bbd320208c3d558199b3
Maxim <jybo@mail.ru>
2023-11-29 10:39:08 +1000

    KS-9-4 вопрос ментору

--- src/main/kotlin/Lesson_9/Les_9_Task_4.kt
+++ src/main/kotlin/Lesson_9/Les_9_Task_4.kt
@@ -12,7 +12,7 @@ package Lesson_9
  */
 
 fun main() {
-    println("Введите 5 ингридиентов")
+    println("Введите 5 ингридиентов, через запятую")
     val listIngrid = mutableListOf<String>()
 
     for (el in 0..4) {
@@ -22,4 +22,15 @@ fun main() {
     val newListIngrid = listIngrid.sorted()
     println()
     print("Отсортированный Список  ${newListIngrid.joinToString(", ")}")
+
+    println()
+    newListIngrid.forEach{el-> println(el) }
 }
+
+/*А как осуществить ввод в одну строку. Если я делаю заполнение, массив с клавиатуры во время создания
+val listIngrid = mutableListOf(readln())
+        ввод происходит как и требуется, но потом почему то не отрабатывают метод сортировки и метод
+        формирование итогового вывода строки (",")
+        В гугле почему то не нашел информацию.
+        можно конечно в группе спросить, но там частенько умничать начинают.
+ */

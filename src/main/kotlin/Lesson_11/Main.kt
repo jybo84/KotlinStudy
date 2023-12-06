f788f4edf6fdf963a271ef910ba86edcf393e70b
Maxim <jybo@mail.ru>
2023-12-06 22:51:08 +1000

    KS-11-1 создание 2-х user-ов

--- src/main/kotlin/Lesson_11/Main.kt
+++ src/main/kotlin/Lesson_11/Main.kt
@@ -5,7 +5,7 @@ fun main() {
     val second = User(id = 2, login = "max", password = "222xxx", email = "max@mail.ru")
 
     println()
-    println("Создан пользователь с id-${first.id} логином-${first.login} паролем-${first.password} и эл.почтой-${first.email}")
-    println()
     println("Создан пользователь с id-${second.id} логином-${second.login} паролем-${second.password} и эл.почтой-${second.email}")
+    println()
+    println("Создан пользователь с id-${first.id} логином-${first.login} паролем-${first.password} и эл.почтой-${first.email}")
 }
--- src/main/kotlin/Lesson_11/User.kt
+++ src/main/kotlin/Lesson_11/User.kt
@@ -1,10 +1,9 @@
 package Lesson_11
 
 class User(
-    val id: Int,
     val login: String,
     val password: String,
     val email: String,
-
+    val id: Int,
 ) {
 }

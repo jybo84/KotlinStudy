1de9393bf3499162fb93863eb036dc5de28fabe8
Maxim <jybo@mail.ru>
2023-12-08 19:22:17 +1000

    KS-11-4 вся кострукция собрана в одном файле (итоговая версия)

--- src/main/kotlin/Lesson_11/Les_11_Task_4.kt
+++ src/main/kotlin/Lesson_11/Les_11_Task_4.kt
@@ -11,7 +11,12 @@ www.figma.com/file/89K5JomHGyIkUFUaBKRBXi/RecipesApp?type=design&node-id=1%3A181
 
 fun main() {
 
-    val categoryOfMenu = Category(listOf("бургер классический", "бургер с грибами"), listOf("Пицца с сыром", "Пицца с грибами"), listOf("шурпа", "борщ"), listOf("оливье", "шуба"))
+    val categoryOfMenu = Category(
+        listOf("бургер классический", "бургер с грибами"),
+        listOf("Пицца с сыром", "Пицца с грибами"),
+        listOf("шурпа", "борщ"),
+        listOf("оливье", "шуба")
+    )
     println("меню супов")
     categoryOfMenu.soupShow()
     println("меню салатов")
@@ -20,10 +25,43 @@ fun main() {
     categoryOfMenu.pizzaShow()
     println("Меню бургеров")
     categoryOfMenu.burgerShow()
-    val ingridients = Ingridients(mapOf("творого/гр" to 350, "яйцо/шт" to 2, "мука/ст.ложки" to 6, "сахар/т.ложки" to 2))
+    val ingridients =
+        Ingridients(mapOf("творого/гр" to 350, "яйцо/шт" to 2, "мука/ст.ложки" to 6, "сахар/т.ложки" to 2))
     println("Ингридиенты для приготовления")
     ingridients.ingridientsShow()
     println("Рецепт приготовления пиццы")
-    val recipe = Recipe( mapOf(1 to "расскатывает тесто", 2 to "мажем кетчуп", 3 to "ложим помидоры и сыр", 4 to "поставить в духовку"))
+    val recipe = Recipe(
+        mapOf(
+            1 to "расскатывает тесто",
+            2 to "мажем кетчуп",
+            3 to "ложим помидоры и сыр",
+            4 to "поставить в духовку"
+        )
+    )
     recipe.reciepOfPizzaShow()
+
+    class Category(
+        val burgers: List<String>,
+        val pizza: List<String>,
+        val soup: List<String>,
+        val salad: List<String>,
+    ) {
+        fun burgerShow() = println(burgers)
+        fun pizzaShow() = println(pizza)
+        fun soupShow() = println(soup)
+        fun saladShow() = println(salad)
+    }
+
+    class Ingridients(
+        val ingridients: Map<String, Int>
+    ) {
+        fun ingridientsShow() = println(ingridients)
+
+    }
+
+    class Recipe(
+        val reciepOfPizza: Map<Int, String>
+    ) {
+        fun reciepOfPizzaShow() = println(reciepOfPizza)
+    }
 }

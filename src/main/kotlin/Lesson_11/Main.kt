d655114ce9e5e4e7334df265fccc80b0af4e3b5c
Maxim <jybo@mail.ru>
2023-12-08 17:34:20 +1000

    KS-11-2 вся кострукция собрана в одном файле (итоговая версия)

--- src/main/kotlin/Lesson_11/Main.kt
+++ src/main/kotlin/Lesson_11/Main.kt
@@ -16,4 +16,40 @@ fun main() {
     max.fixBio()
     max.changePassword()
     max.userInfo()
+
+    class User_2(
+        val id: Int,
+        val login: String,
+        var password: String,
+        val email: String,
+        var bio: String = "",
+    ) {
+        fun userInfo() {
+            println("Информация о пользователе: id-$id, логин-$login, пароль-$password, почта-$email")
+        }
+
+        fun fixBio() {
+            println("Заполните поле bio")
+            bio = readln()
+            println("Информация о пользователе: id-$id, логин-$login, пароль-$password, почта-$email, $bio")
+        }
+
+        fun changePassword() {
+            do {
+                println("Введите Ваш пароль")
+                val usPas = readln()
+                if (usPas != password) {
+                    println("Пароль не совпадает")
+                }
+            } while (usPas != password)
+            println(
+                """
+                Отлично, Вы ввели правильный пароль
+                ПОМЕНЯЙТЕ ПАРОЛЬ
+            """.trimIndent()
+            )
+            password = readln()
+            println("Ваш пароль изменен")
+        }
+    }
 }

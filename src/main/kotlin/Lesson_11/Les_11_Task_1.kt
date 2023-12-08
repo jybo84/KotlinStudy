c679a98116732b02d782bd80189289518c21f21d
Maxim <jybo@mail.ru>
2023-12-08 17:20:05 +1000

    KS-11-4 имитация приложения меню

--- src/main/kotlin/Lesson_11/Les_11_Task_1.kt
+++ src/main/kotlin/Lesson_11/Les_11_Task_1.kt
@@ -9,3 +9,19 @@ package Lesson_11
 
  */
 
+fun main() {
+    val first = User(id = 1, login = "ivan", password = "111zzz", email = "ivan@mail.ru")
+    val second = User(id = 2, login = "max", password = "222xxx", email = "max@mail.ru")
+
+    println()
+    println("Создан пользователь с id-${second.id} логином-${second.login} паролем-${second.password} и эл.почтой-${second.email}")
+    println()
+    println("Создан пользователь с id-${first.id} логином-${first.login} паролем-${first.password} и эл.почтой-${first.email}")
+
+    class User(
+        val login: String,
+        val password: String,
+        val email: String,
+        val id: Int,
+    )
+}

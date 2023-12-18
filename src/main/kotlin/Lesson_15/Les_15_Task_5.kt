4a6094abea301844b82b73fd0394633b9f8fa6f7
Maxim <jybo@mail.ru>
2023-12-18 21:43:07 +1000

    KS-15-5 автомобили/груз

--- src/main/kotlin/Lesson_15/Les_15_Task_5.kt
+++ src/main/kotlin/Lesson_15/Les_15_Task_5.kt
@@ -11,3 +11,56 @@ package Lesson_15
 - интерфейсы должны содержать методы загрузки и разгрузки пассажиров или грузов.
 Спроектируй классы и несколько объектов. Вызови их методы, «перевезя» таким образом 6 человек и 2 тонны груза.
  */
+
+fun main() {
+
+    val car1 = PassengerCar()
+    car1.loadInCar()
+    car1.unloadCar()
+    println()
+
+    val car2 = CargoCar()
+    car2.loadInCar()
+    car2.unloadCar()
+}
+
+
+abstract class Vehicle() : PossibilityTransportCargo, PossibilityTransportHuman {
+
+}
+
+class PassengerCar(override val maxQuantityCargo: Int = 0, override val maxQuantityHuman: Int = 3) : Vehicle(),
+    PossibilityLoad {
+    override fun loadInCar() {
+        println("Я могу в салон посадить $maxQuantityHuman человек")
+    }
+
+    override fun unloadCar() {
+        println("Я на остановке могу высадить $maxQuantityHuman человек")
+    }
+}
+
+class CargoCar(override val maxQuantityCargo: Int = 2, override val maxQuantityHuman: Int = 1) : Vehicle(),
+    PossibilityLoad {
+    override fun loadInCar() {
+        println("Я могу в кузов погрузить $maxQuantityCargo тонн груза и $maxQuantityHuman человек")
+    }
+
+    override fun unloadCar() {
+        println("Я из кузова могу высыпать $maxQuantityCargo тонн песка")
+    }
+}
+
+interface PossibilityTransportHuman {
+    val maxQuantityHuman: Int
+}
+
+interface PossibilityTransportCargo {
+    val maxQuantityCargo: Int
+}
+
+interface PossibilityLoad {
+    fun loadInCar()
+    fun unloadCar()
+}
+

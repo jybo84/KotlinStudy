1c10d70b7af67faf1afab6ee30abb5532de67a53
Maxim <jybo@mail.ru>
2023-12-18 13:44:56 +1000

    KS-13-2 борьба с конфликтами

--- src/main/kotlin/Lesson_13/Les_13_Task-2.kt
+++ src/main/kotlin/Lesson_13/Les_13_Task-2.kt
@@ -14,11 +14,11 @@ package Lesson_13
  */
 
 fun main() {
-    val book = TelephoneBook2("Max", 89241234567)
+    val book = TelephoneBook22("Max", 89241234567)
     book.bookInfo()
 }
 
-class TelephoneBook2(
+class TelephoneBook22(
     val name: String,
     val number: Long,
     val company: String? = null

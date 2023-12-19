251fbe7aebfc9dc2f5b5397eeead3afc8af3b097
Maxim <jybo@mail.ru>
2023-12-20 09:40:01 +1000

    KS-16-3 валидация пароля

---
+++ src/main/kotlin/Lesson_16/Les_16_Task_3.kt
@@ -0,0 +1,36 @@
+package Lesson_16
+
+/*
+Задача 3 к Уроку 16
+
+Создай класс, описывающий пользователя, который хранит поля логин и пароль.
+Пароль не должен быть виден в теле главной функции, но должен быть способ проверить, что пароль введен верно.
+Функция валидации должна принимать строку и возвращать результат проверки (и только его – без распечатки или генерации
+других строк).
+
+Выведи информацию о релевантности пароля в консоль.
+ */
+
+fun main() {
+    val user = User()
+    println("Введите пароль")
+    val userPas = readln()
+    println("Введите логин")
+    val userLogin = readln()
+    user.checkPassword(userPas, userLogin)
+}
+
+class User() {
+
+    private val password = "123"
+    private val login = "Max"
+    fun getPassword() = password
+    fun getLogin() = login
+
+    fun checkPassword(userPas: String, userLogin: String) {
+        if (userPas.equals(getPassword()) && userLogin.equals(getLogin()))
+            println("Валидация прошла успешно")
+        else
+            println("Пароль либо логин неверны")
+    }
+}

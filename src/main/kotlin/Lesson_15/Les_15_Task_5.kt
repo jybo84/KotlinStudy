c75c79918b6035d56a708b9cee90ec50b169f991
Maxim <jybo@mail.ru>
2023-12-21 14:11:42 +1000

    KS-15-5 корректировка по коду

--- src/main/kotlin/Lesson_15/Les_15_Task_5.kt
+++ src/main/kotlin/Lesson_15/Les_15_Task_5.kt
@@ -17,11 +17,13 @@ fun main() {
     val car1 = PassengerCar()
     car1.loadInCar()
     car1.unloadCar()
+    car1.move()
     println()
 
     val car2 = CargoCar()
     car2.loadInCar()
     car2.unloadCar()
+    car2.move()
 }
 
 
@@ -29,38 +31,54 @@ abstract class Vehicle() : PossibilityTransportCargo, PossibilityTransportHuman 
 
 }
 
-class PassengerCar(override val maxQuantityCargo: Int = 0, override val maxQuantityHuman: Int = 3) : Vehicle(),
-    PossibilityLoad {
+class PassengerCar(
+    override val maxLoadCargo: Int = 0,
+    override val maxLoadMen: Int = 3
+) : Vehicle(), PossibilityLoad, Moveble {
     override fun loadInCar() {
-        println("Я могу в салон посадить $maxQuantityHuman человек")
+        println("Я могу в салон посадить $maxLoadMen человек")
     }
 
     override fun unloadCar() {
-        println("Я на остановке могу высадить $maxQuantityHuman человек")
+        println("Я на остановке могу высадить $maxLoadMen человек")
+    }
+
+    override fun move() {
+        println("Я маленький и шустрый. Могу лихачить и быстро ездить")
     }
 }
 
-class CargoCar(override val maxQuantityCargo: Int = 2, override val maxQuantityHuman: Int = 1) : Vehicle(),
-    PossibilityLoad {
+class CargoCar(
+    override val maxLoadCargo: Int = 2,
+    override val maxLoadMen: Int = 1
+) : Vehicle(), PossibilityLoad, Moveble {
     override fun loadInCar() {
-        println("Я могу в кузов погрузить $maxQuantityCargo тонн груза и $maxQuantityHuman человек")
+        println("Я могу в кузов погрузить $maxLoadCargo тонн груза и $maxLoadMen человек")
     }
 
     override fun unloadCar() {
-        println("Я из кузова могу высыпать $maxQuantityCargo тонн песка")
+        println("Я из кузова могу высыпать $maxLoadCargo тонн песка")
+    }
+
+    override fun move() {
+        println("Я большой и неповоротливый. Езжу медленно только по первой полосе")
     }
 }
 
 interface PossibilityTransportHuman {
-    val maxQuantityHuman: Int
+    val maxLoadMen: Int
 }
 
 interface PossibilityTransportCargo {
-    val maxQuantityCargo: Int
+    val maxLoadCargo: Int
 }
 
 interface PossibilityLoad {
     fun loadInCar()
     fun unloadCar()
+}
+
+interface Moveble {
+    fun move()
 }
 

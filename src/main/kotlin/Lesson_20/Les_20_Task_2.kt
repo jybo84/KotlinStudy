8854152fb599951a80bbe193fd32f1b6550fceb2
Maxim <jybo@mail.ru>
2023-12-28 23:42:01 +1000

    KS-20-2 лямды и оъект класса

---
+++ src/main/kotlin/Lesson_20/Les_20_Task_2.kt
@@ -0,0 +1,31 @@
+package Lesson_20
+
+/*
+Задача 2 к Уроку 20
+
+В компьютерной игре игрок может подобрать лечебное зелье, которое восстанавливает всё его здоровье.
+Для реализации программы понадобится создать простой класс игрока (у него должно быть имя, текущее и максимальное
+здоровье).
+– Напиши лямбда-функцию с явным указанием типа переменной, реализующую лечебное зелье. Функция должна принимать
+в качестве параметра объект игрока.
+– Создай экземпляр игрока с неполным здоровьем, примени к нему созданную лямбду-функцию.
+ */
+
+fun main() {
+    val player = Player("Max", 50, 100);
+
+     val lamda: (Player) -> Player
+    lamda = { player: Player -> player.  }            //TODO как здесь получить доступ к внутренностям player ???
+
+
+
+
+    println(player)
+}
+
+class Player(var name: String, var currentHealth: Int, val maxHealth: Int){
+
+    override fun toString(): String {
+        return "Player(name='$name', currentHealth=$currentHealth, maxHealth=$maxHealth)"
+    }
+}

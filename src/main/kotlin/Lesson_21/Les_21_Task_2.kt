e62d365c377c3e6278abdeb7c8442259f102a760
Maxim <jybo@mail.ru>
2024-01-03 18:52:05 +1000

    KS-21-2 расширение List<Int>

--- src/main/kotlin/Lesson_21/Les_21_Task_2.kt
+++ src/main/kotlin/Lesson_21/Les_21_Task_2.kt
@@ -10,8 +10,6 @@ fun main() {
 
     val number = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     println(number.evenNumbersSum())
-    println("_____________-")
-    println(number.evenNumbersSumLamda())  // TODO метод с лямбдой почему то не работает
 }
 
 fun List<Int>.evenNumbersSum(): Int {
@@ -23,15 +21,5 @@ fun List<Int>.evenNumbersSum(): Int {
     return sum
 }
 
-fun List<Int>.evenNumbersSumLamda(): Int {
-    var total = 0
-    var lamda = { list: List<Int>, count: Int ->
-        list.forEach { el ->
-            if (el % 2 == 0) {
-                total += el
-            }
-        }
-    }
-    return total
-}
+
 

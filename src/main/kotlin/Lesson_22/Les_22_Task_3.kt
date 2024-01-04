efbecdbe5cfee0e782fe9c0b10532c9576838de0
Maxim <jybo@mail.ru>
2024-01-04 23:13:20 +1000

    KS-22-3 компаненты объекта

--- src/main/kotlin/Lesson_22/Les_22_Task_3.kt
+++ src/main/kotlin/Lesson_22/Les_22_Task_3.kt
@@ -11,6 +11,14 @@ fun main() {
 
     val volga = Car("Волга", 100, "Черная")
 
+    println(volga.component1())
+    println(volga.component2())
+    println(volga.component3())
+
+    println()
+    println("Либо так")
+    println()
+
     val model = volga.component1()
     val speed = volga.component2()
     val color = volga.component3()

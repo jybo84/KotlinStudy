5167375062b2f1a5ee25d54ad2a560eedbf0e28a
Maxim <jybo@mail.ru>
2024-01-04 18:45:57 +1000

    KS-22-3 параметры объекта Data класса

---
+++ src/main/kotlin/Lesson_22/Les_22_Task_3.kt
@@ -0,0 +1,20 @@
+package Lesson_22
+
+/*
+Задача 3 к Уроку 22
+
+Используя любой data class, в котором есть минимум 3 свойства, выполни деструктуризацию объекта на отдельные
+переменные и выведи их значения в консоль.
+ */
+
+fun main() {
+
+    val volga = Car("Волга", 100, "Черная")
+
+    println(volga.model)
+    println(volga.speed)
+    println(volga.color)
+    println()
+}
+
+data class Car(val model: String, val speed: Int, val color: String)

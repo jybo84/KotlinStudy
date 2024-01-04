0b737ab501e956e030dc955910fbb7f5792771b4
Maxim <jybo@mail.ru>
2024-01-04 19:23:19 +1000

    KS-22-4 метод и data slass

---
+++ src/main/kotlin/Lesson_22/Les_22_task_4.kt
@@ -0,0 +1,39 @@
+package Lesson_22
+
+/*
+Задача 4 к Уроку 22
+
+В разработке Android приложений часто используется паттерн MVVM (Model-View-ViewModel) для разделения бизнес-логики
+и пользовательского интерфейса. ViewModel отвечает за управление состоянием приложения и хранит данные, которые
+необходимы для отображения на экране.
+Предположим, что у нас есть MainScreenViewModel первого экрана приложения, для инициализации которого подгружаются
+данные с сервера. VM хранит единственное свойство mainScreenState.
+ViewModel будет содержать внутренний data class MainScreenState для хранения стейта (состояния). Состояние экрана
+включает два свойства: data (загружаемые данные - String) и isLoading (индикатор процесса загрузки, обычно имеет
+значение по умолчанию false).
+
+Создай data class для хранения состояния. При изменении состояния загрузки создается новый объект стейта, копируя
+текущее состояние с изменением значения поля isLoading.
+Сымитируй процесс загрузки данных с сервера в методе вью модели loadData(), в частности состояния:
+- отсутствие данных;
+- загрузка данных;
+- наличие загруженных данных.
+ */
+
+fun main() {
+
+    val inputDataServer = "2024 год Дракона"
+
+    val mainScreenState = MainScreenState()
+    println(mainScreenState)
+    mainScreenState.loadData(inputDataServer)
+    println(mainScreenState)
+}
+
+data class MainScreenState(var data: String = "данные отсутсвуют", var isLoading: Boolean = false) {
+
+    fun loadData(inputData: String) {
+        data = inputData
+        isLoading = true
+    }
+}

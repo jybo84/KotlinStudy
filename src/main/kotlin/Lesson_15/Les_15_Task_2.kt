3ba6804f62c672c8851ec6f26c5fd1f856ec37d2
Maxim <jybo@mail.ru>
2024-01-10 15:51:33 +1000

    KS-15-2 перегрузка метода

---
+++ src/main/kotlin/Lesson_15/Les_15_Task_2.kt
@@ -0,0 +1,40 @@
+package Lesson_15
+
+/*
+Задача 2 к Уроку 15
+
+Необходимо разработать простую систему мониторинга погодных условий, которая будет собирать данные о температуре
+и количестве осадков.
+Создай абстрактный класс WeatherStationStats. Он будет служить родителем для классов-объектов погодных данных:
+Temperature и PrecipitationAmount.
+
+Реализуй логику имитации отправки сообщения на сервер в классе WeatherServer. Метод отправки сообщения должен
+определять тип переданного сообщения и в зависимости от результата передавать соответствующие данные.
+
+Создай по одному объекту для разных типов погодных данных и протестируй работу программы.
+ */
+
+fun main() {
+
+    val weatherServer = WeatherServer()
+    weatherServer.sendMassageOnServer(2.0)
+    weatherServer.sendMassageOnServer(5)
+}
+
+abstract class WeatherStationStats()
+
+open class Temperature(private val temp: Int) : WeatherStationStats()
+
+
+open class PrecipitationAmount(private val amount: Double) : WeatherStationStats()
+
+class WeatherServer() {
+    fun sendMassageOnServer(temp: Int) {
+        println("Данные о температуре: $temp")
+    }
+
+    fun sendMassageOnServer(amount: Double) {
+        println("Данные о количестве осадков: $amount")
+    }
+}
+

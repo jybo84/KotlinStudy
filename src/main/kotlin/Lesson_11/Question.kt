1ced5a7faefd4ca6d57e001f70b60675f481b7d2
Maxim <jybo@mail.ru>
2024-01-14 22:12:10 +1000

    KS-13-5 воспроизведение ошибки NumberFormatException

--- src/main/kotlin/Lesson_11/Question.kt
+++ src/main/kotlin/Lesson_11/Question.kt
@@ -6,13 +6,14 @@ fun main() {
     println(first)
     println()
 
-    val second = Second(111, "Max")
+    val second = Second("Макс")
+    println(second)
 }
 
 data class First( val userId: Int = (0..100).random()) //TODO здесь нормально
 
 
-data class Second(val userId: Int = (0..100).random(), val name: String) // TODO здесь не хочет
+data class Second(val name: String, val userId: Int = (0..100).random()) // TODO здесь не хочет
 
 
 

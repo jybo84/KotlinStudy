73cfa41f01b976187b095f0d18c3fb4d7a3a5818
Maxim <jybo@mail.ru>
2024-01-18 23:29:02 +1000

    KS-11-4 доработка кода

--- src/main/kotlin/Lesson_11/Les_11_Task_4.kt
+++ src/main/kotlin/Lesson_11/Les_11_Task_4.kt
@@ -12,57 +12,33 @@ www.figma.com/file/89K5JomHGyIkUFUaBKRBXi/RecipesApp?type=design&node-id=1%3A181
  */
 
 fun main() {
-    val dish1 = Menu(
-        Category.BURGERS.title, NameOfDish.CLASSICAL_BURGER.title, Ingredients().classicalBurgerIngredients
+
+    val burger = Category(
+        "БУРГЕРЫ", "булка с котлетой",
+        Recipe(
+            "классический", Ingredients(
+                listOf("майонез", "кетчуп", "булка", "котлета"),
+                "выкладываем слоями"
+            )
+        )
     )
 
-    val dish2 = Menu(
-        Category.SOUP.title, NameOfDish.CHICKEN_SOUP.title, Ingredients().chickenSoupIngredients
+    burger.printDish()
+}
+
+data class Category(val name: String, val description: String, val recipe: Recipe)
+
+
+data class Recipe(val nameDish: String, val ingredients: Ingredients)
+
+
+data class Ingredients(val list: List<String>, val descriptionCooking: String)
+
+
+fun Category.printDish() {
+    println(
+        "КАТЕГОРИЯ: $name \nОПИСАНИЕ БЛЮДА: $description  \nНАЗВАНИЕ БЛЮДА: ${recipe.nameDish} " +
+                "\nИНГРИДИЕНТЫ: ${(recipe.ingredients.list).joinToString(" ")} " +
+                "\nРЕЦЕПТ ПРИГОТОВЛЕНИЯ: ${recipe.ingredients.descriptionCooking}"
     )
-
-    println(dish1)
-    println()
-    println(dish2)
-
-}
-
-data class Menu(
-
-    val category: String,
-    val nameOfDish: String,
-    val ingredients: List<String>,
-
-    ) {
-    override fun toString(): String {
-        return "категория: $category \nназвание блюда: $nameOfDish \nИнгридиенты: ${ingredients.joinToString(", ")}"
-    }
-}
-
-enum class Category(var title: String) {
-    BURGERS("БУРГЕРЫ"),
-    PIZZA("ПИЦЦЦА"),
-    SOUP("СУП"),
-    SALAD("САЛАТ"),
-}
-
-enum class NameOfDish(var title: String) {
-    CLASSICAL_BURGER("Бургер классический"),
-    MUSHROOM_BURGER("Бургер с грибами"),
-    CLASSICAL_PIZZA("Классичееская пицца"),
-    MUSHROOM_PIZZA("Грибная пицца"),
-    CHICKEN_SOUP("Куринный суп"),
-    BORSCH("Борщ"),
-    OLIVE("Оливье"),
-    SALAD_SEA_CABBAGE("Салат из морской капусты"),
-}
-
-class Ingredients() {
-    val classicalBurgerIngredients = listOf("булка", "котлета", "кетчуп", "майонез")
-    val mushroomBurgerIngredients = listOf("булка", "котлета", "кетчуп", "майонез", "грибы")
-    val classicPizzaIngredients = listOf("тесто", "колбаса", "сыр", "помидоры")
-    val mushroomPizzaIngredients = listOf("тесто", "колбаса", "сыр", "помидоры", "грибы")
-    val chickenSoupIngredients = listOf("вода", "соль", "яйцо", "курица")
-    val borschIngredients = listOf("капуста", "картоха", "свекла", "мясо", "вода", "соль")
-    val oliveIngredients = listOf("картоха", "горошек", "огурец", "морковка")
-    val saladSeaCabbageIngredients = listOf("морская капуста")
 }
